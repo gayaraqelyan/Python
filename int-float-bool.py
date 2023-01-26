0b572c7673b77a35f00df730526aa0ea944d09b5
Gayane <pyaraqelyan@gmail.com>
2023-01-26 12:15:55 +0400

    My work

---
+++ int-float-bool.py
@@ -0,0 +1,52 @@
+# -------------------------------------------Exercises 1--------------------------------------
+# Define a variable of type integer
+
+# x = 100
+
+# Define a variable of type float
+# y = 3.5
+# z = y + x
+# print(z)
+
+# Define a variable of type boolean
+
+# a = x != y or x >= y or x <= y
+# a1 = not (x != y or x >= y or x <= y)
+# b = x != y and x >= y and x <= y
+# b1 = not (x != y and x >= y and x <= y)
+
+# print(a)
+# print(a1)
+# print(b)
+# print(b1)
+
+# Print int, float, and bool variables:
+# print(f"int:\t{x}\nfloat:\t{y}\nbool:\t{a} {a1} {b} {b1}")
+
+# ---------------------------------------Exercises 2------------------------------------------------
+# Define a Boolean type variable
+
+# bool_val = False
+# bool_val=int(bool_val==False)
+# print(bool_val+3)
+
+# Define a Boolean type variable
+
+# bool_Val = True
+
+# convert it to an integer type
+
+# bool_Val=int(bool_Val==False)
+
+# add 3 to it, print the result.
+
+# print(bool_Val+3)
+# -------------------------------------Exercises 3---------------------------------------------------
+# Define a string type variable
+
+# str_var = "1"
+
+#  add to it a string variable with the value "2",
+
+# str_var1 = str_var + "2"
+# print(str_var1)
