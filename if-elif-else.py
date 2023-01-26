a84696df4d00f705547a51ef6855f796c56bd135
Gayane <pyaraqelyan@gmail.com>
2023-01-26 12:14:04 +0400

    My work

---
+++ if-elif-else.py
@@ -0,0 +1,72 @@
+# ----------------------------------------Exercises 1--------------------------------------------
+# I wrote a program that determines whether it is even or odd,
+# multiple of 3 or not, divides by 3 and 2 at the same time.
+
+# num = int(input("Please,enter the number:\t"))
+#
+# if num % 2 == 0:
+#     print("This is number even")
+#     if num % 2 == 0 and num % 3 == 0:
+#         print("this number divided by 2 and 3")
+#     else:
+#         print("No divided")
+# else:
+#     print("This is number odd")num1=int(input("Please, enter the frist number:\t"))
+# num2=int(input("Please, enter the second number:\t"))
+# print("The large number:\t",max(num1,num2))
+# ---------------------------------------------------version 2-----------------------------------------------------
+# if num1>=num2:
+#     print("num1 is greater than num2")
+# else:
+#     print("The large num2")
+
+# -----------------------------------------------Exercises 2----------------------------------------------------
+
+
+
+
+# ------------------------------------------------Exercises 3---------------------------------------------------
+# a=15
+# b=97
+# if a>=b:
+#     print("a is greater than b")
+# else:
+#     print(f"The large b")
+
+# ------------------------------------------------Exercises 4-----------------------------------------------------
+# Input number from consol and check
+# num =int(input("Please ,enter the number:\t"))
+#
+# if num%5==0:
+#     print("Number divides by 5")
+# elif num%5==0 and num%7==0:
+#     print("Number divides by 5 and 7")
+# else:
+#     print("Number divides by 7")
+# --------------------------------------------------Exercises 5------------------------------------------------
+# Input a string from console and count a length of the string
+#
+# name_of_string=input("Please, enter the word:\t")
+#
+# if len(name_of_string)<10:
+#     print("Short word",name_of_string.count("a"))
+# elif len(name_of_string)>10:
+#     print("long word",name_of_string.count("a"))
+# else:
+#     print("too long word")
+
+# -----------------------------------------------------Exercises 6-------------------------------------------------
+# Calculate the sum of all numbers given in yhe list.
+#
+# List_of_numbers=[1,12,3,14,5,16]
+
+# print(sum(List_of_numbers))
+
+# sumarry=0
+# for i in List_of_numbers:
+#     sumarry=sumarry+i
+# print("Sum:\t",sumarry)
+
+
+
+
